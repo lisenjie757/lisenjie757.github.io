51f21f211d5c966417f96aadd4b437df4c4a0a94
lisenjie757 <1215954303@qq.com>
2023-04-15 16:12:03 +0800

    sas

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -11,10 +11,10 @@ export default defineUserConfig({
   // 网站语言，默认为中文
   lang: "zh-CN",
   // 网站标题
-  title: "LearnData-开源笔记",
+  title: "Li Senjie",
   // 网站描述
   description:
-    "开源工具、效率方法、心理学探索的自我提升笔记，记录并输出一切能让自己提升的知识。",
+    "浙江大学硕士",
 
   // 网站路径默认为主域名。如果网站部署在子路径下，比如 xxx.com/yyy，那么 base 应该被设置为 "/yyy/"
   base: "/",
