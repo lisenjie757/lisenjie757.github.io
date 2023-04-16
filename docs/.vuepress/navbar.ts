3f77e894a0d71fb94970c0495a6aabd38e02fc54
lisenjie757 <1215954303@qq.com>
2023-04-16 17:07:01 +0800

    v5.3

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -9,7 +9,6 @@ export default navbar([
     icon: "study",
     link: "/knowledge",
     prefix: "/knowledge/",
-    children: "structure"
   },
 
   { text: "工具收藏", icon: "tool", link: "https://nav.newzone.top/" },
